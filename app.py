c55baf49ba1e483d77e9d8d9ae318726f122f696
Govind <govind-devops@DESKTOP-894AGL4.localdomain>
2026-03-03 18:34:13 +0000

    Test 1: Only app.py changed, should be FAST

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Docker CI Build Optimization - Day 28"
+    return "Docker CI Build Optimization "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
+"# Testing Cache: No logic change"
